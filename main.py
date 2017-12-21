5fc59f5de132b1f6712ec16aab5bbdc54b42e56c
Misha <stun007@gmail,com>
2017-12-22 02:33:47 +0300

    +

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ class Node:
         self.name = name
 
 
-root_tree = Node(None, 'root', [], 'root')
-active_node = root_tree
+root_tree = [Node(None, 'root', [], 'root'), Node(None, 'root', [], 'root')]
+active_node = [root_tree[0], root_tree[1]]
 
 
 def print_root(root, out_file=sys.stdout):
@@ -41,36 +41,36 @@ def del_type(root, node_type):
         i += 1
 
 
-def analysis_str(string=''):
-    global active_node
+def analysis_str(string, ind_root):
+    a_n = active_node[ind_root]
     if string[0] == '/' and string[1] == '/':
-        active_node.body.append(Node(active_node, 'comment', [], string[:-1]))
+        a_n.body.append(Node(a_n, 'comment', [], string[:-1]))
         return
     string = re.sub(' *\n *', ' ', string)
     if string[-1] == '}':
-        active_node = active_node.prev
+        a_n = a_n.prev
     elif string[-1] == '{':
         if 'if' in string:
-            active_node.body.append(Node(active_node, 'if', [], string[:-1]))
+            a_n.body.append(Node(a_n, 'if', [], string[:-1]))
         elif 'while' in string:
-            active_node.body.append(Node(active_node, 'while', [], string[:-1]))
-        elif '=' not in string or string.find('=') > string.find('('):
-            active_node.body.append(Node(active_node, 'function', [], string[:-1]))
-        active_node = active_node.body[-1]
+            a_n.body.append(Node(a_n, 'while', [], string[:-1]))
+        elif '(' in string and ('=' not in string or string.find('=') > string.find('(')):
+            a_n.body.append(Node(a_n, 'function', [], string[:-1]))
+        a_n = a_n.body[-1]
     elif string[-1] == ';':
         if 'if' in string:
-            active_node.body.append(Node(active_node, 'if', [], string))
+            a_n.body.append(Node(a_n, 'if', [], string))
         elif 'while' in string:
-            active_node.body.append(Node(active_node, 'while', [], string))
-        elif len(string.split('=')[0].strip().split()) == 2:
-            active_node.body.append(Node(active_node, 'variable', [], string))
+            a_n.body.append(Node(a_n, 'while', [], string))
+        elif len(string.split('(')[0].split('=')[0].strip().split()) == 2:
+            a_n.body.append(Node(a_n, 'variable', [], string))            # FIX_ME
         else:
-            active_node.body.append(Node(active_node, 'other', [], string))
+            a_n.body.append(Node(a_n, 'other', [], string))
     elif string[-1] == '/' and string[-2] == '*':
-        active_node.body.append(Node(active_node, 'comment', [], string[string.find("/*"):]))
+        a_n.body.append(Node(a_n, 'comment', [], string[string.find("/*"):]))
 
 
-def analysis_file(file_path):
+def analysis_file(file_path, ind_root):
     # init
     separate1 = [';', '{', '}']
     separate2 = ['//', '*/']
@@ -87,18 +87,18 @@ def analysis_file(file_path):
     # ---------
     while True:
         if string_file[id_sep] in separate1:
-            analysis_str(string_file[id_prev_sep:id_sep + 1])
+            analysis_str(string_file[id_prev_sep:id_sep + 1], ind_root)
             id_prev_sep = id_sep + 1
         elif id_sep + 1 != len_string_file and string_file[id_sep:id_sep + 2] in separate2:
             if string_file[id_sep:id_sep + 2] == '//':
                 i = id_sep
                 while string_file[i] != '\n':
                     i += 1
-                analysis_str(string_file[id_sep:i + 1])
+                analysis_str(string_file[id_sep:i + 1], ind_root)
                 len_string_file -= len(string_file[id_sep:i + 1])
                 string_file = string_file[:id_sep] + string_file[i + 1:]
             else:
-                analysis_str(string_file[id_prev_sep:id_sep + 2])
+                analysis_str(string_file[id_prev_sep:id_sep + 2], ind_root)
                 id_sep += 1
             id_prev_sep = id_sep + 1
         id_sep += 1
@@ -117,10 +117,10 @@ def rename_variables(root):
     if root.type == 'variable':
         variables = str(root.name).replace(';', '').split(',')
         try:
-            rename_node_in_depth(root.prev, variables[0].split()[1], 'v' + str(rename_variables.count_rename))
+            rename_node_in_depth(root, variables[0].split()[1], 'v' + str(rename_variables.count_rename))
         except AttributeError:
             rename_variables.count_rename = 0
-            rename_node_in_depth(root.prev, variables[0].split()[1], 'v' + str(rename_variables.count_rename))
+            rename_node_in_depth(root, variables[0].split()[1], 'v' + str(rename_variables.count_rename))
         rename_variables.count_rename += 1
         for i in range(1, len(variables)):
             rename_node_in_depth(
@@ -134,10 +134,10 @@ def rename_functions(root):
     if root.type == 'function':
         func_name = str(root.name).split('(')[0].split()[-1]
         try:
-            rename_node_in_depth(root.prev, func_name, 'f' + str(rename_functions.count_rename))
+            rename_node_in_depth(root, func_name, 'f' + str(rename_functions.count_rename))
         except AttributeError:
             rename_functions.count_rename = 0
-            rename_node_in_depth(root.prev, func_name, 'f' + str(rename_functions.count_rename))
+            rename_node_in_depth(root, func_name, 'f' + str(rename_functions.count_rename))
         rename_functions.count_rename += 1
         rename_args = str(root.name).split('(')[1].split(',')
         for arg in rename_args:
@@ -146,17 +146,26 @@ def rename_functions(root):
             while not a[arg_name].isalpha():
                 arg_name -= 1
             arg_name = a[arg_name]
-            rename_node_in_depth(root.prev, arg_name, 'f' + str(rename_functions.count_rename))
+            rename_node_in_depth(root, arg_name, 'f' + str(rename_functions.count_rename))
             rename_functions.count_rename += 1
     for i in root.body:
         rename_functions(i)
 
 
 if __name__ == '__main__':
-    analysis_file('quick-sort.cpp')
-    del_type(root_tree, 'comment')
-    rename_functions(root_tree)
-    rename_variables(root_tree)
-    file = open('out.cpp', 'w')
-    print_root(root_tree, file)
-    file.close()
+    analysis_file('quick-sort.cpp', 0)
+    analysis_file('merge-sort.cpp', 1)
+    del_type(root_tree[0], 'comment')
+    rename_functions(root_tree[0])
+    rename_variables(root_tree[0])
+
+    del_type(root_tree[1], 'comment')
+    rename_functions(root_tree[1])
+    rename_variables(root_tree[1])
+
+    file0 = open('out0.cpp', 'w')
+    file1 = open('out1.cpp', 'w')
+    print_root(root_tree[0], file0)
+    print_root(root_tree[1], file1)
+    file0.close()
+    file1.close()
